8e263ac55e8dfc9d4c7401d78a5896419a41ea54
BAR-KEY <park96h@naver.com>
2022-02-04 05:36:11 +0900

    dice 페이지 디자인 구현

--- lib/pages/dice.dart
+++ lib/pages/dice.dart
@@ -5,6 +5,50 @@ class Dice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('hi');
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text('Dice game'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Image.asset('image/dice1.png'),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Image.asset('image/dice2.png'),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          ButtonTheme(
+              minWidth: 100,
+              height: 60,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orangeAccent,
+                  ),
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.play_arrow,
+                    color: Colors.white,
+                    size: 50,
+                  )))
+        ],
+      )),
+    );
   }
 }
